3898974ae0d9126774904490769b101097801f89
Lorena Arabia <56326571+Larabia@users.noreply.github.com>
2020-09-17 12:23:14 -0300

    se agregan paquetes de datos y entidades, se genera la clase persona con sus constructores, getters y setters y metodo tostrng.

--- ManejoJDBC/src/main/java/datos/Conexion.java
+++ ManejoJDBC/src/main/java/datos/Conexion.java
@@ -1,9 +1,32 @@
 package datos;
 
+import java.sql.*;
+
 public class Conexion {
 
 	private static final String JDBC_URL = "jdbc:MySql://localhost:3306/test?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
 	private static final String JDBC_ADMIN = "root";
     private static final String JDBC_PASS = "";
+    
+    public static Connection getConnection() throws SQLException {
+    	return DriverManager.getConnection(JDBC_URL,JDBC_ADMIN, JDBC_PASS);
+    }
 	
+    public static void close(ResultSet rs) throws SQLException {
+    	rs.close();
+    }
+    
+    public static void close(Statement stmt) throws SQLException {
+    	stmt.close();
+    }
+    
+    public static void close(PreparedStatement stmt) throws SQLException {
+    	stmt.close();
+    }
+    
+    public static void close(Connection con) throws SQLException {
+    	con.close();
+    }
+    
+    
 }
