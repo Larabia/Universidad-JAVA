530eb8c6b7de402ddfe7909e448369ece9720180
Lorena Arabia <56326571+Larabia@users.noreply.github.com>
2020-09-17 17:24:12 -0300

    se testea la insercion de personas en la clase TestManejoPersonas

--- ManejoJDBC/src/main/java/test/TestManejoPersonas.java
+++ ManejoJDBC/src/main/java/test/TestManejoPersonas.java
@@ -7,12 +7,22 @@ import entidades.Persona;
 public class TestManejoPersonas {
 	public static void main(String[] args) {
 		PersonaDAO personaDao = new PersonaDAO();
-		List<Persona> personas= personaDao.seleccionar();
 		
+		
+//		Insertar persona a la tabla
+		Persona personaNueva = new Persona("Carlos", "Esparza", "carlos@gamil.com", "2423423423");
+		personaDao.insertar(personaNueva);
+		
+//		Listar personas de la tabla
+		
+		List<Persona> personas= personaDao.seleccionar();		
 		for(Persona persona:personas) {
 			System.out.println("Persona: "+persona);
 			
 		}
+		
 	}
+	
+	
 
 }
