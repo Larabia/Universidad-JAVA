f301b3f6fbe525ce1ca46353a2124e4333a7fbed
Lorena Arabia <56326571+Larabia@users.noreply.github.com>
2020-09-17 11:53:33 -0300

    proyecto de introduccion y de manejo de JDBC

---
+++ ManejoJDBC/src/main/java/datos/Conexion.java
@@ -0,0 +1,9 @@
+package datos;
+
+public class Conexion {
+
+	private static final String JDBC_URL = "jdbc:MySql://localhost:3306/test?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+	private static final String JDBC_ADMIN = "root";
+    private static final String JDBC_PASS = "";
+	
+}
